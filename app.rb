6eba6b80c644081a0ef5c3ad44987a9c7c4293e8
David Lewis and Jared Clemmensen <jared@>
2018-02-12 11:58:08 -0800

    add input form and style

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 also_reload('lib/**/*.rb')
-require './lib/triangles'
+require './lib/parcel'
 
 get ('/') do
-  
+  erb(:input)
+end
+
+get('/parcel') do
+  length = params.fetch('length')
+  width = params.fetch('width')
+  height = params.fetch('height')
+  weight = params.fetch('weight')
+  shipping = params.fetch('shipping')
+  insurance = params.fetch('insurance')
+  parcel = Parcel.new(length, width, height, weight)
+  @cost = parcel.cost_to_ship(shipping,insurance)
+  erb(:output)
 end
--- lib/parcel.rb
+++ lib/parcel.rb
@@ -10,9 +10,20 @@ class Parcel
     @vol = @l * @w * @h
   end
 
-  def cost_to_ship
-    base = 5
-    base += @vol * 0.05
-    base += @weight * 0.05
+  def cost_to_ship(shipping,insurance)
+    if shipping == 'air'
+      base = 10
+      base += @weight * 0.15
+    elsif shipping == 'ground'
+      base = 5
+      base += @weight * 0.05
+    elsif shipping == 'overnight'
+      base = 300
+      base += @weight * 1
+    end
+
+    if insurance
+      base += 15
+    end
   end
 end
