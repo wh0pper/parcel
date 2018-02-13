a6d32999fce1ce4f96310287827eebe48cd2574a
David Lewis and Jared Clemmensen <david@>
2018-02-12 16:58:06 -0800

    work on incorporating google maps api for distance

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,3 +1,8 @@
+require 'net/http'
+require 'json'
+
+#AIzaSyDzSzDb3XUuK54_3vFQoY-kbDD_p6_qGDc
+
 class Parcel
   def initialize(l, w, h, weight)
     @l = l.to_i
@@ -26,4 +31,27 @@ class Parcel
       base += 15
     end
   end
+
+  def distance(zip1, zip2)
+
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=97211&destinations=97218&key=AIzaSyDzSzDb3XUuK54_3vFQoY-kbDD_p6_qGDc"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+
+    result = JSON.dump(response)
+    resultarray = result.split(':')
+    puts resultarray[6].gsub(/^(?!\d(.*?)\mi$)/,'')
+
+    # result.each do |result|
+    #   puts result
+    # end
+    # rows = object['rows']
+    # puts rows.fetch('elements')
+    # # distance = elements['distance']
+    # # puts final = distance['text']
+
+  end
 end
+
+parcel = Parcel.new(1, 1, 1, 1)
+parcel.distance(97211, 97218)
